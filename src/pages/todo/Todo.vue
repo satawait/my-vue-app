aa24c5737f05260330a219d2587adb8f24476955
liuxingbang <liuxingbang@migu.cn>
2022-06-29 09:36:13 +0800

    fix: study

--- src/pages/todo/Todo.vue
+++ src/pages/todo/Todo.vue
@@ -10,7 +10,7 @@
 import { ref } from 'vue'
 import { useMouse } from '@vueuse/core'
 const upload = ref()
-const { x, yy } = useMouse()
+const { x, y } = useMouse()
 const changeFile = (e: Event) => {
   console.log(e)
 }
